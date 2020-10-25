1ba59d5d0c9971d842d7558c6262e4fbc00cff03
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-10-25 15:03:15 +0000

    Updates

--- d64684d8-6caee0eaa523d60d6454.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{PRV2:function(t,a,n){"use strict";n.d(a,"a",(function(){return o}));var r=n("Lnxd");function o(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor"},child:[{tag:"path",attr:{d:"M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"}},{tag:"path",attr:{d:"M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"}}]})(t)}}}]);
-//# sourceMappingURL=d64684d8-6caee0eaa523d60d6454.js.map
