bb69acc81a277feccd41281c35b91b3f5ff5ac7b
ken <kenjoo.dev@gmail.com>
2022-05-20 17:14:43 +0100

    created stock and gold array

--- src/pages/allWeatherPorfolio.js
+++ src/pages/allWeatherPorfolio.js
@@ -5,16 +5,42 @@ import { Helmet } from 'react-helmet';
 
 
 export default () => {
-  const [weeklyHigh, setWeeklyHigh] = useState({});
+  const [stockWeeklyHigh, setStockWeeklyHigh] = useState({});
+  const [goldWeeklyHigh, setGoldWeeklyHigh] = useState({});
   const [cost, setCost] = useState(10000);
   const [stock, setStock] = useState("VUSA.LON");
   const [bond, setBond] = useState("");
-  const [gold, setGold] = useState("");
+  const [gold, setGold] = useState("SGLD.LON");
   const [duration, setDuration] = useState(52);
+
   useEffect(() => {
-     const getStock = async() => await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${stock}&apikey=MC9ST99K30HA6HRB`).then(res => res.json()).then(data => setWeeklyHigh(data["Weekly Time Series"]));
+    const getStock = async() => await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${stock}&apikey=MC9ST99K30HA6HRB`).then(res => res.json()).then(data => setStockWeeklyHigh(data["Weekly Time Series"]));
+    const getGold = async() => await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${gold}&apikey=MC9ST99K30HA6HRB`).then(res => res.json()).then(data => setGoldWeeklyHigh(data["Weekly Time Series"]));
     getStock();
+    getGold();
   },[])
+
+  let combinedArr = [];
+  if (stockWeeklyHigh != null && goldWeeklyHigh != null) {
+    for (let i=0; i<Object.entries(stockWeeklyHigh).length; i++) {
+      let date, stockPrice, goldPrice;
+      if (Object.entries(stockWeeklyHigh)[i] != undefined) {
+        date = Object.entries(stockWeeklyHigh)[i][0];
+        stockPrice = Object.entries(stockWeeklyHigh)[i][1]["2. high"];
+      }
+      if (Object.entries(goldWeeklyHigh)[i] != undefined) {
+        goldPrice = Object.entries(goldWeeklyHigh)[i][1]["2. high"];
+      }
+      const combinedObj = {
+        "date":date,
+        "stock":stockPrice,
+        "gold":goldPrice,
+      }
+      combinedArr.push(combinedObj);
+    }
+  }
+
+  console.log(combinedArr);
   
   return (
     <>
@@ -24,12 +50,16 @@ export default () => {
           <th>Week</th>
           <th>High</th>
           <th>Cost</th>
+          <th>Stock</th>
+          <th>Bond</th>
+          <th>Gold</th>
+          <th>Cash</th>
           <th>Profit</th>
         </tr>
         <tbody>
       {
-       Object.entries(weeklyHigh).map((week,index) => {
-        const startHigh = Object.entries(weeklyHigh)[duration-1][1]["2. high"];
+       Object.entries(stockWeeklyHigh).map((week,index) => {
+        const startHigh = Object.entries(stockWeeklyHigh)[duration-1][1]["2. high"];
         const sharenum = cost/startHigh;
         if (index < duration){
           const high = week[1]["2. high"];
